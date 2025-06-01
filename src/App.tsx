8b71e4525c8d81ce9b461ba89c2abd07bc585945
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-06-01 01:35:25 -0600

    themes ig

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div id='App' className='flex h-screen w-screen'>
-      <header></header>
+      <header className=''></header>
       <article></article>
       <footer></footer>
     </div>
